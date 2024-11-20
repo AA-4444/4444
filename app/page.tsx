a877a1169df6829949fda9412093241aaca60a0d
Alex <104495043+AA-4444@users.noreply.github.com>
2024-11-20 18:37:33 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,287 @@
+// app/page.tsx
+'use client'; // This tells Next.js that this component should be rendered on the client side
+
+import React, { useState, useEffect } from "react";
+import "./globals.css"; // Use relative path
+import Link from "next/link";
+
+
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading screen for 3 seconds (you can change the duration as needed)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // 3000ms = 3 seconds
+}, []);
+
+useEffect(() => {
+  setIsClient(true)
+}, [])
+
+
+  return (
+    <div className="App">
+      {/* Loader Screen */}
+      {isLoading && (
+        <div className="loader-screen">
+          <div className="loader"></div>
+        </div>
+      )}
+
+      {/* Content Section - Visible after loading */}
+      {!isLoading && (
+        <section className="Welcome-section">
+          <div className="content">
+            <h1 className="name">Welcome</h1>
+            <p className="title">This is my developer coze corner</p>
+
+            {/* Scroll Indicator */}
+            <div className="mouse_scroll">
+              <div className="mouse">
+                <div className="wheel"></div>
+              </div>
+              <div>
+                <span className="m_scroll_arrows unu"></span>
+                <span className="m_scroll_arrows doi"></span>
+                <span className="m_scroll_arrows trei"></span>
+              </div>
+            </div>
+
+            {/* Social Icons */}
+            <div className="social-icon-container">
+              <div className="icon-circle">
+                <a
+                  href="https://www.linkedin.com/in/alex-zarytskyi-7b2248193/"
+                  className="social-icon linkedin"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
+            <div className="social-icon-container">
+              <div className="icon-circle">
+                <a
+                  href="https://github.com/AA-4444"
+                  className="social-icon github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+            <div className="social-icon-container">
+              <div className="icon-circle">
+                <a
+                  href="https://x.com/Alex34909682"
+                  className="social-icon twitter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+              </div>
+            </div>
+            <div className="social-icon-container">
+              <div className="icon-circle">
+              <a
+  href="#"
+  onClick={() => {
+    window.location.href = "mailto:" + atob("YWE0NDQ0Y29udGFjdEBnbWFpbC5jb20=");
+  }}
+  className="social-icon mail"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <i className="fas fa-envelope"></i>
+</a>
+
+              </div>
+            </div>
+          </div>
+        </section>
+      )}
+
+      {/* About me section */}
+      <section className="aboutme">
+        <div className="header-2">
+          <h1>About me</h1>
+          <p>
+            I'm a software developer from Kyiv, Ukraine currently residing in Wroclaw, Poland. I specialize in SwiftUI and Swift, crafting elegant iOS applications. I'm also a web developer, blending functionality with aesthetic design. You can contact me and I can serve you in English, Polish, Ukrainian and implement your ideas.
+          </p>
+        </div>
+      </section>
+
+      {/* Animated Programming Languages Line */}
+      <div className="ProgLine">
+        <div className="header">
+          <h2>Skills & Tools</h2>
+        </div>
+
+        <div className="scroll-container">
+          <div className="carousel-primary">
+            <i className="fab fa-swift"></i>
+            <i className="fab fa-js-square"></i>
+            <i className="fab fa-html5"></i>
+            <i className="fab fa-css3-alt"></i>
+            <i className="fab fa-php"></i>
+            <i className="fab fa-node-js"></i>
+            <i className="fab fa-react"></i>
+            <i className="fas fa-database"></i>
+            <i className="fas fa-fire"></i>
+            <i className="fab fa-figma"></i>
+          </div>
+          <div className="carousel-primary carousel-secondary">
+            <i className="fab fa-swift"></i>
+            <i className="fab fa-js-square"></i>
+            <i className="fab fa-html5"></i>
+            <i className="fab fa-css3-alt"></i>
+            <i className="fab fa-php"></i>
+            <i className="fab fa-node-js"></i>
+            <i className="fab fa-react"></i>
+            <i className="fas fa-database"></i>
+            <i className="fas fa-fire"></i>
+            <i className="fab fa-figma"></i>
+          </div>
+        </div>
+      </div>
+
+      {/* Career Timeline Section */}
+      <div className="header">
+        <h1>Career Timeline</h1>
+      </div>
+      <ul className="timeline">
+        <li>
+          <div className="direction-r">
+            <div className="flag-wrapper">
+              <span className="flag">Student</span>
+              <span className="time-wrapper"><span className="time">2022 - present</span></span>
+            </div>
+            <div className="desc">Student of Computer Science in University of Opole</div>
+          </div>
+        </li>
+
+        <li>
+          <div className="direction-l">
+            <div className="flag-wrapper">
+              <span className="flag">Freelancer</span>
+              <span className="time-wrapper"><span className="time">2023 - 2024</span></span>
+            </div>
+            <div className="desc">Freelancer as web developer</div>
+          </div>
+        </li>
+
+        {/* Project */}
+        <li>
+          <div className="direction-r">
+            <div className="flag-wrapper">
+              <span className="flag">AVA100</span>
+              <span className="time-wrapper"><span className="time">2024 - 2024</span></span>
+            </div>
+            <div className="desc">Developed a website for the Ukranian building company AVA100</div>
+          </div>
+        </li>
+
+
+        <li>
+          <div className="direction-l">
+            <div className="flag-wrapper">
+              <span className="flag">IOS Speedtracker</span>
+              <span className="time-wrapper"><span className="time">2023 - 2024</span></span>
+            </div>
+            <div className="desc">
+        Creating IOS <Link href="/speedometr" className="underline-link">Speedtracker</Link> that helps users track their speed and route path (with an option for app customization).
+      </div>
+
+
+          </div>
+        </li>
+
+
+        <li>
+          <div className="direction-r">
+            <div className="flag-wrapper">
+              <span className="flag">DemaxDigital</span>
+              <span className="time-wrapper"><span className="time">2023 - 2024</span></span>
+            </div>
+            <div className="desc">Working as junior IOS Developer</div>
+          </div>
+        </li>
+
+        {/* More items here */}
+        <li>
+          <div className="direction-l">
+            <div className="flag-wrapper">
+              <span className="flag">TapConGroup</span>
+              <span className="time-wrapper"><span className="time">2024 - 2024</span></span>
+            </div>
+            <div className="desc">Co-founder of a startup of a company that is engaged in tech development</div>
+          </div>
+        </li>
+
+        <li>
+		<div className="direction-r">
+			<div className="flag-wrapper">
+				<span className="flag">NeoBrutalism</span>
+				<span className="time-wrapper"><span className="time">2024 - 2024</span></span>
+			</div>
+			<div className="desc">Creating Web Site   <a href="https://aa-4444.github.io/Neo-Brutalism/" className="underline-link">Neo-Brutalism</a> to tell about Neo-Brutalism style in web design</div>
+		</div>
+	</li>
+
+
+  <li>
+		<div className="direction-l">
+			<div className="flag-wrapper">
+				<span className="flag">Demo Crypto App</span>
+				<span className="time-wrapper"><span className="time">2024 - 2024</span></span>
+			</div>
+			<div className="desc">Creating small   <Link href="/crypto" className="underline-link">Crypto App</Link> on SwiftUI ,using API for showing real time value of different crypto</div>
+		</div>
+	</li>
+
+  <li>
+		<div className="direction-r">
+			<div className="flag-wrapper">
+				<span className="flag">IOS-Developer</span>
+				<span className="time-wrapper"><span className="time">2023 - 2024</span></span>
+			</div>
+			<div className="desc">Working as IOS-Develper in startup,developing App using SwiftUI</div>
+		</div>
+	</li>
+
+  <li>
+		<div className="direction-l">
+			<div className="flag-wrapper">
+				<span className="flag">Plant App</span>
+				<span className="time-wrapper"><span className="time">2022 - 2022</span></span>
+			</div>
+			<div className="desc">Creating small <Link href="/plant-app" className="underline-link">PLant App</Link> using SwiftUI ,its small app creating as demo e-commerce app where user can choose and by plants for their home</div>
+		</div>
+	</li>
+
+  <li>
+		<div className="direction-r">
+			<div className="flag-wrapper">
+				<span className="flag">KPI</span>
+				<span className="time-wrapper"><span className="time">2021 - 2022</span></span>
+			</div>
+			<div className="desc">Student of the faculty of system analysis</div>
+		</div>
+	</li>
+</ul>
+
+        {/* Footer */}
+        <section className="foot">
+          <p>All rights reserved © 2024</p>
+        </section>
+      
+    </div>
+  );
+}
---
+++ app/speedometr/page.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import React from "react";
+
+export default function Speedometr() {
+  return (
+    <div id="main-content" className="main-content">
+        {/* Background container */}
+        <div className="background"></div>
+
+        <div className="content">
+            <h1>Speedometer</h1>
+            <div className="project-video">
+                <video id="projectVideo" autoPlay loop muted playsInline>
+                    <source src="/speedometr1.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <button id="toggleButton">Pause</button>
+                <a href="/" className="back">Back to Home</a>
+            </div>
+        </div>
+
+        {/* CSS Styling directly in the file */}
+        <style jsx>{`
+        .main-content {
+          margin: 0;
+          padding: 0;
+          display: grid;
+          place-items: center;
+          height: 100vh;
+          text-align: center;
+          position: relative;
+          overflow: hidden;
+          width: 100vw
+        }
+
+        /* Background div */
+        .background {
+        position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+     background: linear-gradient(
+              90deg,
+              color-mix(in lch, canvasText, lightgray 70%) 1px,
+              transparent 1px 45px
+            ) 50% 50% / 45px 45px,
+            linear-gradient(color-mix(in lch, canvasText, lightgray 70%) 1px, transparent 1px 45px)
+              50% 50% / 45px 45px;
+          mask: linear-gradient(-20deg, transparent 50%, white);
+          z-index: -1; /* Keeps background behind content */
+          pointer-events: none; /* Prevents it from interfering with other elements */
+        }
+
+        .content {
+            padding: 20px;
+            
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+        }
+
+        .content h1 {
+            color: white;
+            font-size: 2.5rem;
+            text-align: center;
+        }
+
+        .line {
+            width: 100%;
+            height: 2px;
+            background-color: #10633D;
+            margin: 20px 0;
+        }
+
+        .project-video {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 100%;
+            max-width: 1000px;
+            overflow: hidden;
+            border-radius: 15px;
+        }
+
+        .project-video video {
+            width: 100%;
+            max-height: 60vh;
+            clip-path: inset(0 round 15px);
+        }
+
+        .project-video button {
+            margin-top: 20px;
+            padding: 10px 20px;
+            font-size: 16px;
+            color: black;
+            background-color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        .project-video button:hover {
+            background-color: #EFB8C7;
+            color: white;
+        }
+            .back {
+             display: block;
+            margin-top: 2%;
+            color: white;
+            text-decoration: none;
+            }
+
+        @media (max-width: 768px) {
+            .content h1 {
+                font-size: 1.6rem !important;
+            }
+            .project-video video {
+                max-height: 40vh;
+            }
+        }
+
+          @media (max-width: 480px) {
+            .content h1 {
+                font-size: 1.8rem !important;
+            }
+            .project-video video {
+                max-height: 50vh;
+            }
+                .back {
+                margin-top: 5%;
+                }
+        }
+        `}</style>
+    </div>
+  );
+}
